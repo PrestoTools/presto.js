8cc814d15cbddb73eec38886fe4a60e32aaf14ed
Yu Watanabe <watanabe.yu.1203@gmail.com>
2018-09-28 10:39:46 +0900

    Refactoring server-php sample
    Add README.md, divide long function.

--- example/server-php/index.php
+++ example/server-php/index.php
@@ -16,19 +16,18 @@
 		$file_size = intval($_POST['size']);
 
 		$save_dir = '../tmp';
-		$save_part_dir = $save_dir.'/part';
-		$save_file_path = $save_part_dir.'/'.$file_id.'.part'.$_POST['prestoChunkIndex'];
+		$save_file_path = $save_dir.'/part/'.$file_id.'.part'.$_POST['prestoChunkIndex'];
 		if (!is_dir($save_dir)) {
 			mkdir($save_dir, 0777, true);
 		}
-		if (is_dir($save_dir) && !is_dir($save_part_dir)) {
-			mkdir($save_part_dir, 0777, true);
+		if (is_dir($save_dir) && !is_dir($save_dir.'/part')) {
+			mkdir($save_dir.'/part', 0777, true);
 		}
 
 		if (move_uploaded_file($file['tmp_name'], $save_file_path)) {
 			$total_chunk_number = intval($_POST['totalChunkNumber']);
 			$part_file_count = 0;
-			foreach(scandir($save_part_dir) as $part_file) {
+			foreach(scandir($save_dir.'/part') as $part_file) {
 				if (stripos($part_file, $file_id) !== false) {
 					$part_file_count++;
 				}
@@ -38,7 +37,7 @@
 				die();
 			}
 
-			createFileFromChunks($save_part_dir, $save_dir, $file_name, $file_id, $file_size);
+			createFileFromChunks($save_dir, $file_name, $file_id, $file_size);
 			header($_SERVER['SERVER_PROTOCOL'] . ' 200 OK', true, 200);
 
 			die();
@@ -48,37 +47,43 @@
 		}
 	}
 
-	function createFileFromChunks($save_part_dir, $save_dir, $file_name, $file_id, $file_size) {
-
+	function createFileFromChunks($save_dir, $file_name, $file_id, $file_size) {
 		$total_files = 0;
-		foreach(scandir($save_part_dir) as $file) {
+		foreach(scandir($save_dir.'/part') as $file) {
 			if (stripos($file, $file_id) !== false) {
 				$total_files++;
 			}
 		}
 
-		$delete_file_list = array();
-		if (($fp = fopen($save_dir.'/'.$file_name, 'w')) !== false) {
-			for ($i = 0; $i < $total_files; $i++) {
-				$part_file = $save_part_dir.'/'.$file_id.'.part'.$i;
-				fwrite($fp, file_get_contents($part_file));
-				array_push($delete_file_list, $part_file);
-			}
-			fclose($fp);
-		} else {
+		$fp = fopen($save_dir.'/'.$file_name, 'w');
+		if ($fp === false) {
 			header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 			die();
 		}
 
+		$delete_file_list = array();
+		for ($i = 0; $i < $total_files; $i++) {
+			$part_file = $save_dir.'/part/'.$file_id.'.part'.$i;
+			fwrite($fp, file_get_contents($part_file));
+			array_push($delete_file_list, $part_file);
+		}
+		fclose($fp);
+
+		checkFileSize($save_dir, $file_name, $file_size);
+		deletePartFiles($delete_file_list);
+	}
+
+	function checkFileSize($save_dir, $file_name, $file_size) {
 		if (filesize($save_dir.'/'.$file_name) !== $file_size) {
 			header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 			die();
 		}
+	}
 
+	function deletePartFiles($delete_file_list) {
 		foreach($delete_file_list as $delete_file) {
 			if (file_exists($delete_file)) {
 				@unlink($delete_file);
 			}
 		}
-
 	}
